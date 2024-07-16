99cc7a597aab654d0402b7183d21c8733e33947b
Aman Jain <aman.codes0@gmail.com>
2024-07-17 02:56:02 +0530

    debug env vars

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,30 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  private JWT_SECRET: string;
-
-  private JWT_EXPIRATION_TIME: string;
-
-  private CLIENT_URL: string;
-
-  private GOOGLE_CLIENT_ID: string;
-
-  private GOOGLE_CLIENT_SECRET: string;
-
-  private NODE_ENV: string;
-
-  constructor(private readonly configService: ConfigService) {
-    this.JWT_SECRET = configService.get('JWT_SECRET');
-    this.JWT_EXPIRATION_TIME = configService.get('JWT_EXPIRATION_TIME');
-    this.CLIENT_URL = configService.get('CLIENT_URL');
-    this.GOOGLE_CLIENT_ID = configService.get('GOOGLE_CLIENT_ID');
-    this.GOOGLE_CLIENT_SECRET = configService.get('GOOGLE_CLIENT_SECRET');
-    this.NODE_ENV = configService.get('NODE_ENV');
-  }
-
   getHello(): string {
-    return `Hello World! ${this.JWT_SECRET} ${this.JWT_EXPIRATION_TIME} ${this.NODE_ENV} ${this.CLIENT_URL}`;
+    return 'Hello World!';
   }
 }
