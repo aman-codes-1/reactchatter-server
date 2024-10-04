1af16b003ab4fe6b8326d42691d9d51a2e672371
Aman Jain <amanjain@Amans-MacBook-Pro.local>
2024-10-05 01:28:24 +0530

    updated npm packages, migrated to eslint v9

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-param-reassign */
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
